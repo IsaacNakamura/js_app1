fbb4661ead090c965857e06e29a4350e905cf81a
AISAKU NAKAMURA <aisakunakamura@AISAKUs-MacBook-Pro.local>
2019-06-08 16:27:43 -0400

    css added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// https://www.youtube.com/embed/LFa9fnQGb3g このビデオ
+
 function revealMessage(){
 	document.getElementById("hiddenMessage").style.display = 'block' ;
 }
+
+// https://www.youtube.com/embed/JjIvF0yikGU このビデオ
+
+$(document).ready(function(){
+	$("#hidden").hover(function(){
+		$(this).css("color","black");	
+	},
+	function(){
+		//$(this).hide();		
+		//$(this).css("display","none");
+		$(this).css("color","white");
+	});
+	
+	$("#button2").click(function(){
+		alert("I am alerting you!気をつけて!")
+	});
+});
